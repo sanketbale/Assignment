8ac4eeddf659e4c6339564914dcd29c707b6cd1f
Sanket Bale <sanketb.124@gmail.com>
2020-12-18 12:12:12 +0530

    BT

---
+++ BTPanorama/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+	$('#hamburger-button').click(function () {
+		$(this).toggleClass('open');
+	});
+
+	$('.add-on img').click(function () {
+		$(this).toggleClass('open');
+	});
+
+	$('.pack-details-table').toggle();
+
+	$('.card.pack').click(function () {
+		$('.pack-details-table').toggle();
+		if (!$(this).hasClass('flip')) {
+			$('.card.pack').removeClass('flip');
+			$(this).toggleClass('flip');
+		} else {
+			$(this).toggleClass('flip');
+		}
+	});
+
+	$('.toggle .item').click(function () {
+		$('.toggle .item').toggleClass('active');
+	});
+
+
+	$('.individual-report').click(function () {
+		if ($(this).hasClass('open')) {
+			$(this).removeClass('open');
+			$(this).children().removeClass('open');
+			$(this).addClass('closed');
+			$(this).children().addClass('closed');
+		} else {
+			$(this).addClass('open');
+			$(this).children().addClass('open')
+		};
+		
+		var id =  $(this).attr('data-target');
+
+		if($('#'+id).hasClass('open')) {
+			$('#'+id).removeClass('open');
+			$('#'+id).height(0);
+		  } else {
+			$('#'+id).addClass('open');
+			$('#'+id).height('60px');
+		  }
+	});
+
+});
